e3ceb81268a494b0d2f3ada2e6335775d49c6b7f
thefunkymonk13 <michael.reilly13@gmail.com>
2024-03-30 15:58:38 -0400

    First commit

---
+++ maiden/src/app/components/header.js
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <> 
+        <div class="main">
+            <div class="nav">
+                <div class="nav-row">
+                    <h1 class="title">Maiden, America</h1>
+                    <button class="login-button shadow"><a href="login">Login</a></button>
+                </div>
+            </div>
+        </div>
+        </>
+     );
+}
+
+export default Header;
